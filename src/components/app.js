31ecc7fbf7231cf0a2c21859cd15b8f53d1e6e13
NathanielYoung <young.nathaniel95@gmail.com>
2019-05-18 19:10:35 -0600

    MainMenu setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import * as actions from '../reducers/actions';
+// import Footer from './parts/footer';
+// import NavBar from './parts/navBar';
+import { Link } from 'react-router-dom';
+import ContainedButtons from './parts/button'
+import Notifcation from './parts/notificationButton'
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+      <div className='mainMenu'>
+        <div id='title'>
+          WWOutpost 2
+        </div>
+        <div id='mainMenuCenter'>
+          <div id='mainMenuLogin'>
+            {ContainedButtons('Login')}
+          </div>
+          <div id='welcome'>
+            Welcome User
+          </div>
+          <div id='newGamebtn'>
+            {ContainedButtons('New Game')}
+          </div>
+          <div id='continueGamebtn'>
+            {ContainedButtons('Continue Game')}
+            <div id='gamesReadyForYou'>
+              {Notifcation(3)}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
